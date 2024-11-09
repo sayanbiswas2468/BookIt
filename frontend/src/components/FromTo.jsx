6ce825aa8dc95e28b506a3fe2bd968bd059f30f5
Sayan Biswas <sayanbiswas2468@gmail.com>
2024-11-09 14:34:15 +0530

    implement dashboard

--- frontend/src/components/FromTo.jsx
+++ frontend/src/components/FromTo.jsx
@@ -5,10 +5,17 @@ import dayjs from 'dayjs'
 import { useState } from 'react'
 import TrainDest from '../../assets/icons/Dest.svg'
 import TrainSrc from '../../assets/icons/Src.svg'
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 const FromTo = () => {
     const [from, setFrom] = useState("")
     const [to, setTo] = useState("")
     const [date, setDate] = useState(dayjs())
+    const swapFromTo = () => {
+        setFrom(prevFrom => {
+            setTo(prevFrom);  
+            return to;         
+        });
+    };
     return (
         <div className=' p-10 rounded-2xl backdrop-blur-lg top-[40%] relative w-[90%] ml-[5%] pt-[5rem]'>
             <div className=" bg-white flex flex-row p-7 rounded-[30px] text-[20px] justify-center opacity-100 blur-none">
@@ -19,6 +26,10 @@ const FromTo = () => {
                         onChange={(e) => { setFrom(e.target.value) }}
                     />
                 </form>
+
+                <button className='ml-[2.3rem]' onClick={swapFromTo}>
+                    <CompareArrowsIcon sx={{ fontSize: 30 }} />
+                </button>
 
                 <form className='flex flex-row ml-10 rounded-l '>
                     <label for="name" ><img src={TrainDest} alt="Train" width={40} className=' ml-2 mt-2' /></label>
